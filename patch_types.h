2dce384d6ab10e4f3d8bff137501075273d5b86b
Floogle <18466542+skyfloogle@users.noreply.github.com>
2021-03-23 23:31:25 +0100

    massive restructure to make adding patches easier

---
+++ patch_types.h
@@ -0,0 +1,35 @@
+#ifndef PATCH_TYPES_H
+#define PATCH_TYPES_H
+
+#include <stdbool.h>
+
+typedef struct {
+	int pos;
+	int orig_byte;
+	int new_byte;
+} PatchByte;
+
+typedef enum {
+	UPX,
+	JOY,
+	MEM,
+	DPLAY,
+	SCHED,
+	INPUTLAG,
+	TYPE_COUNT,
+} PatchType;
+
+typedef enum {
+	UNFOUND, // default aka 0
+	ABLE,
+	DONE,
+} PatchState;
+
+typedef struct {
+	PatchByte *bytes;
+	char *name;
+	PatchType type;
+	PatchState state;
+} Patch;
+
+#endif
